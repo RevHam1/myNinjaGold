2f4a88568e99026dec9f19cdd0a3c77853b2204b
RevHam1 <36148347+RevHam1@users.noreply.github.com>
2025-03-28 17:11:53 -0500

    Modified urls.py

--- myninja_gold/myninja_gold_app/urls.py
+++ myninja_gold/myninja_gold_app/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from myninja_gold_app.views import index
 
+from . import views
+
 urlpatterns = [
-    path('', index),
+    # path('', index),
+    path('', views.index, name='index'),
 ]
